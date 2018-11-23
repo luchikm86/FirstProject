1b9f71feffc53c34e77169c0b9e31f8c2c48773a
luchikm86 <luchik.m86@gmail.com>
2018-11-23 10:44:16 +0200

    5.2 - successful completion

--- src/lesson5/hw/CustomerWithdraw.java
+++ src/lesson5/hw/CustomerWithdraw.java
@@ -7,32 +7,26 @@ public class CustomerWithdraw {
         String[] names = {"Jack", "Ann", "Denis", "Andrey", "Nikolay", "Irina", "John"};
         int[] balances = {100, 500, 8432, -99, 12000, -54, 0};
 
-        int bal = withdraw(names, balances, "Ann", 1400);
+        int bal = withdraw(names, balances, "Ann", 400);
         System.out.println(bal);
         System.out.println(Arrays.toString(balances));
     }
 
     static int withdraw(String[] clients, int[] balances, String client, int amount){
-
-        int clientIndex = 0;
-        for (String cl : clients){
-            if (cl == client){
+        int clI = 0;
+        for (String elem : clients){
+            if (elem.equals(client)){
                 break;
             }
-            clientIndex++;
+            clI++;
         }
+        int bal = balances[clI];
+        if (amount > bal){
+            return -1;
+        }else {
 
-        int bal = balances[clientIndex];
-
-        for (int elem : balances){
-            if (elem == bal) {
-                if (amount > bal){
-                    return -1;
-                }
-                    bal -= amount;
-            }
         }
-        int b = balances[clientIndex] = bal;
-        return b;
+       int result = balances[clI] - amount;
+        return result;
     }
 }
