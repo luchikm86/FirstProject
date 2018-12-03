cbf1425600da309480da767be8ac5182e5fefe0d
luchikm86 <luchik.m86@gmail.com>
2018-12-03 13:41:14 +0200

    lesson8 -hw 2

--- src/lesson8/hw/ex2/Demo.java
+++ src/lesson8/hw/ex2/Demo.java
@@ -1,12 +1,25 @@
 package lesson8.hw.ex2;
 
+
+
 public class Demo {
+
     public static Student createHighestParent(){
-        return new Student("Maksym", "Luchenko", 8, null);
+        Student student = new Student("Max", "Luchenko", 8, new Course[1]);
+
+        return student;
+
+        //кратко
+//        return new Student("Max", "Luchenko", 8, new Course[1]);
     }
 
     public static SpecialStudent createLowestChild(){
-        return new SpecialStudent("Sergry", "Apalko", 8, null, 6104, "students@kneu.ua");
+        SpecialStudent specialStudent = new SpecialStudent("Ivan", "Chonkin", 8, new Course[1], 6104, "ivan@gmail.com");
+
+        return specialStudent;
+
+        //кратко
+//        return new SpecialStudent("Ivan", "Chonkin", 8, new Course[1], 6104, "ivan@gmail.com");
     }
 
 }
