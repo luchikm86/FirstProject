21a42c86582b71ca81d1596c3dadc49b20591c88
luchikm86 <luchik.m86@gmail.com>
2018-12-15 03:47:40 +0200

    lesson11 - hw1

--- src/lesson11/hw/BookingComAPI.java
+++ src/lesson11/hw/BookingComAPI.java
@@ -11,7 +11,7 @@ public class BookingComAPI implements API{
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         int count = 0;
         for (Room room : rooms) {
-            if (room != null && price > 0 && room.getPrice() >= price - 100 &&
+            if (room.getPrice() >= price - 100 &&
                     room.getPrice() <= price + 100 && room.getPersons() == persons &&
                     room.getCityName().equals(city) && room.getHotelName().equals(hotel)) {
                 count++;
@@ -21,7 +21,7 @@ public class BookingComAPI implements API{
         Room[] result = new Room[count];
         int index = 0;
         for (Room room : rooms) {
-            if (room != null && price > 0 && room.getPrice() >= price - 100 &&
+            if (room.getPrice() >= price - 100 &&
                     room.getPrice() <= price + 100 && room.getPersons() == persons &&
                     room.getCityName().equals(city) && room.getHotelName().equals(hotel)) {
                 result[index] = rooms[index];
--- src/lesson11/hw/Demo.java
+++ src/lesson11/hw/Demo.java
@@ -19,8 +19,5 @@ public class Demo {
 
         GoogleAPI googleAPI = new GoogleAPI(rooms);
 
-
-
-
     }
 }
