9400ea83415e82044dd64a7ed0d3eb66f2d1661b
luchikm86 <luchik.m86@gmail.com>
2018-12-15 04:24:47 +0200

    lesson11 - hw1

--- src/lesson11/hw/BookingComAPI.java
+++ src/lesson11/hw/BookingComAPI.java
@@ -11,16 +11,16 @@ public class BookingComAPI implements API{
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-//        int count = 0;
-//        for (Room room : rooms) {
-//            if (room != null && room.getPrice() >= price - 100 &&
-//                    room.getPrice() <= price + 100 && room.getPersons() == persons &&
-//                    room.getCityName().equals(city) && room.getHotelName().equals(hotel)) {
-//                count++;
-//            }
-//        }
+        int count = 0;
+        for (Room room : rooms) {
+            if (room != null && room.getPrice() >= price - 100 &&
+                    room.getPrice() <= price + 100 && room.getPersons() == persons &&
+                    room.getCityName().equals(city) && room.getHotelName().equals(hotel)) {
+                count++;
+            }
+        }
 
-        Room[] result = new Room[rooms.length];
+        Room[] result = new Room[count];
         int index = 0;
         for (Room room : rooms) {
             if (room != null && room.getPrice() >= price - 100 &&
