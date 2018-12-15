e0ba714e1e8d91ef523de37f8913cc2a7c05b29d
luchikm86 <luchik.m86@gmail.com>
2018-12-15 04:53:06 +0200

    lesson11 - hw1

--- src/lesson11/hw/Controller.java
+++ src/lesson11/hw/Controller.java
@@ -31,23 +31,36 @@ public class Controller {
     }
 
     public Room[] check(API api1, API api2) {
-//        int count = 0;
-//        for (API api : apis) {
-//            if (api != null && api1.equals(api2)) {
-//                count++;
-//            }
-//        }
-//
-//        Room[] result = new Room[count];
-//        int index = 0;
-//        for (API api : apis){
-//            if (api != null && api1.equals(api2)) {
-//                for (Room room : api1.getAll())
-//                result[index] = room;
-//                index++;
-//            }
-//        }
-//        return result;
-        return null;
+        int count = 0;
+        for (Room room : api1.getAll()) {
+            for (Room room1 : api2.getAll()) {
+                if (room != null && room1 != null) {
+                    if (room.getPrice() == room1.getPrice() &&
+                            room.getPersons() == room1.getPersons() &&
+                            room.getCityName().equals(room1.getCityName()) &&
+                            room.getHotelName().equals(room1.getHotelName())) {
+                        count++;
+                    }
+                }
+            }
+
+        }
+
+        Room[] result = new Room[count];
+        int index = 0;
+        for (Room room : api1.getAll()) {
+            for (Room room1 : api2.getAll()) {
+                if (room != null && room1 != null) {
+                    if (room.getPrice() == room1.getPrice() &&
+                            room.getPersons() == room1.getPersons() &&
+                            room.getCityName().equals(room1.getCityName()) &&
+                            room.getHotelName().equals(room1.getHotelName())) {
+                        result[index] = room;
+                    }
+                        count++;
+                    }
+                }
+            }
+        return result;
     }
 }
