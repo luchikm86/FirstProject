cec21ad30a2e385a9c60da5459062921b5613136
luchikm86 <luchik.m86@gmail.com>
2019-01-06 14:05:55 +0200

    12

--- src/lesson12/UkrainianBankSystem.java
+++ src/lesson12/UkrainianBankSystem.java
@@ -14,9 +14,33 @@ public class UkrainianBankSystem implements BankSystem {
         user.setBalance(user.getBalance() - amount - amount * user.getBank().getCommission(amount));
     }
 
+    /* итак. метод fund. пополнение.Логика:
+     1. Проверяет, не превышен ли лимит на пополнение. Расчитывает по формуле: сумма пополнения +
+     сумма за комиссию на пополнение.
+     2. Записывает в новый баланс значение: знач. текущего баланса + сумма пополнения - сумма комиссии
+     на зачисление.*/
     @Override
     public void fund(User user, int amount) {
-        //TODO homework - fund - пополнение
+        if (!checkFunding(user, amount))
+            return;
+        else //if (amount - (amount * user.getBank().getCommission(amount)) < user.getBank().getLimitOfFunding())
+            user.setBalance(user.getBalance() + amount);
+    }
+
+    private boolean checkFunding(User user, int amount) {
+        return (checkFundingLimits(user, amount, user.getBank().getLimitOfFunding()));
+    }
+
+    private boolean checkFundingLimits(User user, int amount, double limit) {
+        if (amount > limit) {
+            printFundingErrorMsg(user, amount);
+            return false;
+        }
+        return true;
+    }
+
+    private void printFundingErrorMsg(User user, int amount) {
+        System.err.println("Can't replenish on " + amount + " of user " + user.toString());
     }
 
     @Override
@@ -26,15 +50,23 @@ public class UkrainianBankSystem implements BankSystem {
 
         if (!checkWithdraw(fromUser, amount))
             return;
-        //TODO check fund rules
 
-        fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
-        //TODO fund
+//
+//        fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
+
+        if(fromUser.getBank().getCurrency() == toUser.getBank().getCurrency()) {
+
+            withdraw(fromUser, amount);
+        } else return;
+
+        fund(toUser, amount);
     }
 
     @Override
     public void paySalary(User user) {
-        //TODO homework
+        if (user.getSalary() > user.getBank().getLimitOfFunding()) {
+            return;
+        } else user.setBalance(user.getBalance() + user.getSalary());
     }
 
     private boolean checkWithdraw(User user, int amount) {
